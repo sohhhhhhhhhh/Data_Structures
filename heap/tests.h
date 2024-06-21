06ea878a444d3937d57ae4b737d0375b67a3e9cc
sohhhhhhhhhh <sofikamakno@gmail.com>
2024-06-21 16:28:39 +1000

    Add tests

--- heap/tests.h
+++ heap/tests.h
@@ -1,13 +1,39 @@
 #pragma once
-
 #include "heap.h"
 
-void test_remove_from_empty() {
-    Heap_max h({1, 2, 300});
+void test_duplicate_elements() {
+    Heap_max h;
+    for (int i = 0; i < 100; ++i) {
+        h.push(42);
+    }
+    while (h.len() > 0) {
+        std::cout << h.del() << " ";
+    }
+    std::cout << std::endl;
 }
 
-void test_add_one_value_and_print() {}
+void test_add_one_value_and_print() {
+    Heap_max h;
+    h.push(10);
+    h.print_heap();
+}
 
-void test_add_random_values_and_print() {}
+void test_descending_order_elements() {
+    Heap_max h;
+    for (int i = 100; i >= 0; --i) {
+        h.push(i);
+    }
+    h.print_heap();
+}
 
-void test_add_and_remove_random_values_and_print() {}
+void test_push_pop_alternating() {
+    Heap_max h;
+    for (int i = 0; i < 50; ++i) {
+        h.push(i);
+        if (i % 2 == 0) {
+            std::cout << h.del() << " ";
+        }
+    }
+    std::cout << std::endl;
+    h.print_heap();
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
+#include "tests.h"
+
 #include <iostream>
+#include <iostream>
+#include<iostream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    test_remove_from_empty();
+    test_add_one_value_and_print();
+    test_duplicate_elements();
+    test_descending_order_elements();
+    test_push_pop_alternating();
     return 0;
 }
